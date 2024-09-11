e8bc788618ff149aaca1082e1b7c10d4e8e731c0
Zeliang-Codetech <Zeliangcodetech@gmail.com>
2024-09-11 12:19:40 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+<center>
+<hr>
+
+		<footer>
+           <p>All Rights Reserved 2016 by waqasmaqbool.com </p>
+        <footer>
+</center>
---
+++ footer_index.php
@@ -0,0 +1,5 @@
+<center>
+		<footer>
+           <p>All Rights Reserved 2016  by waqasmaqbool.com</p>
+        <footer>
+</center>
